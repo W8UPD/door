cdab0de00bd96056bc547bb2dcca6d8e42c525ea
Ricky Elrod <ricky@elrod.me>
2013-04-01 21:00:53 -0400

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='door',
+      version='1.0',
+      description='Door Code',
+      author='Ricky Elrod',
+      author_email='ricky@elrod.me',
+      url='https://github.com/W8UPD/door',
+      packages=['door'],
+)

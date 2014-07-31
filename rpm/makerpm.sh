e171c169c43b4099a5a2d84ec54db377bc833f4c
Ricky Elrod <ricky@elrod.me>
2014-07-31 00:42:54 -0400

    this never got committed
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- rpm/makerpm.sh
+++ rpm/makerpm.sh
@@ -2,6 +2,26 @@
 
 cwd="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
 
+if [ "$1" == "clean" ]; then
+  rmcommand="rm -rfv $cwd/door-master* $cwd/noarch $cwd/*.src.rpm"
+  echo "About to run: $rmcommand"
+  echo "Press control-c to abort, or enter to continue."
+  read
+  $rmcommand
+  exit
+fi
+
+if [ "$1" == "fetch" ]; then
+  wget -O "$cwd"/door-master.tar.gz https://github.com/W8UPD/door/archive/master.tar.gz
+  if [ $? == 0 ]; then
+    echo "You should now be able to just run ./makerpm.sh to build the RPM"
+    exit 0
+  else
+    echo "Unable to fetch sources. Try again later?"
+    exit 1
+  fi
+fi
+
 rpmbuild --define "_sourcedir ." --define "_rpmdir ." --define "_builddir `pwd`" --define "_srcrpmdir ." --define "_speccdir ." -ba "$cwd"/door.spec
 
 if [ $? == 0 ]; then
